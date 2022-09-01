70a51c9ce38a40a273696ff48ac24c3b5b83145f
swaya <swaya@DESKTOP-H27PH04>
2022-09-01 17:30:30 +0530

    commit and push

---
+++ Pattern/src/Pattern.java
@@ -0,0 +1,18 @@
+public class Pattern{
+	public static void main(String[] args) {
+		int i;
+		int j;
+		
+		for(i=0; i<4; i++)
+		{
+			System.out.println("*");
+		}
+		for(j=0; j<=0; j++)
+		{
+			System.out.println("* * * * *");
+		}
+	}
+	
+}
+
+
